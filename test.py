e504697a45da894bc6c098223912a9bf515df44b
colin-lag <34300599+colin-lag@users.noreply.github.com>
2018-03-28 17:16:39 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,35 +6,35 @@
 # License libre CC
 # Colin Laganier - Thomas Le Menestrel - 2018.03.27
 
-#Importation des bibliothÃ¨ques nÃ©cessaires
+#Importation des bibliothèques nécessaires
 import pygame
 from pygame.locals import *
 from random import randint
 
-#Initialisation de la bibliothÃ¨que Pygame
+#Initialisation de la bibliothèques Pygame
 pygame.init()
 
 #CrÃ©ation de la fenÃªtre
 fenetre = pygame.display.set_mode((700, 700))
 
-#On donne un nom Ã  la fenÃªtre
+#On donne un nom à la fenêtre
 pygame.display.set_caption("Snake")
 
-#On charge un fond noir avec lequel on remplit la fenÃªtre
+#On charge un fond noir avec lequel on remplit la fenêtre
 couverture = pygame.Surface(fenetre.get_size())
 couverture = couverture.convert()
 couverture.fill((250, 250, 250))
 fenetre.blit(couverture, (0,0))
 
-# On charge un personnage qu'on colle sur la fenÃªtre par dessus le fond noir
+# On charge un personnage qu'on colle sur la fenêtre par dessus le fond noir
 Personnage1 = pygame.image.load("F:\ISN\head.png").convert_alpha()
 Personnage1 = pygame.transform.scale(Personnage1, (30,30))
 fenetre.blit(Personnage1, (0,0))
 
-# On rÃ©cupÃ¨re sa position
+# On récupère sa position
 position_perso1 = Personnage1.get_rect()
 
-#RafraÃ®chissement de l'Ã©cran
+#Rafraichissement de l'écran
 pygame.display.flip()
 
 #Variable qui continue la boucle
@@ -75,10 +75,10 @@ while(continuer):
 #Variable qui continue la boucle
 continuer = True
 
-# On re-colle la fenÃªtre
+# On re-colle la fenêtre
 fenetre.blit(couverture, (0,0))
 
-# On recolle le personnage Ã  sa nouvelle position
+# On recolle le personnage à sa nouvelle position
 fenetre.blit(Personnage1, position_perso1)
 
 pygame.display.flip()
