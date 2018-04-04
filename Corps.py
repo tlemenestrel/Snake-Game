7ba92547a39e8df13fe86b8e9a5d7dbb9418f224
tlemenestrel <34300678+tlemenestrel@users.noreply.github.com>
2018-04-04 10:03:10 +0200

    Create Corps.py

---
+++ Corps.py
@@ -0,0 +1,7 @@
+class Corps():
+    def position ():
+        self.position_2 = corps1.get_rect()
+    def load ():
+        #On charge le corps du serpent
+        corps1 = pygame.image.load("corps.png").convert_alpha()
+        corps1 = pygame.transform.scale(corps1, (25,25))
