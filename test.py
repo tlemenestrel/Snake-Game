e9a22e2db44c0364968493fbc142b918236a5ae2
colin-lag <34300599+colin-lag@users.noreply.github.com>
2018-04-16 10:52:51 +0200

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,9 @@ from random import randint
 
 x = [0]
 y = [0]
-step = 10
+step = 8
+score = 0
+length = 3
 
 #Création d'un grand nombre de rangs au sein de la liste pour éventuellement agrandir le corps du serpent jusqu'à 1000 sections
 for i in range(0,1000):
@@ -26,7 +28,7 @@ def collision(x1,y1,x2,y2, size_snake, size_fruit):
 		if ((y1 >= y2) or (y1 + size_snake >=y2)) and y1 <= y2 + size_fruit:
 			return True
 		return False
-	
+
 #Fonction qui affiche le score du joueur sur la page de jeu	
 def disp_score(score):
     font = pygame.font.SysFont(None, 25)
@@ -43,43 +45,32 @@ fenetre_rect = fenetre.get_rect()
 #On donne un nom à la fenêtre
 pygame.display.set_caption("Snake")
 
-#On charge la musique du jeu
-musique= pygame.mixer.music.load("musique.mp3")
-pygame.mixer.music.play()
-
 #On charge un fond noir avec lequel on remplit la fenêtre
 couverture = pygame.Surface(fenetre.get_size())
 couverture = couverture.convert()
 couverture.fill((250, 250, 250))
 fenetre.blit(couverture, (0,0))
 
-# On charge un personnage qu'on colle sur la fenêtre par dessus le fond noir
+#On charge les images des différents objets du jeu
 head = pygame.image.load("head.png").convert_alpha()
 head = pygame.transform.scale(head, (35,35))
-fenetre.blit(head, (0,0))
-
 corps1 = pygame.image.load("corps.png").convert_alpha()
 corps1 = pygame.transform.scale(corps1, (25,25))
-fenetre.blit(corps1, (5,35))
-corps2 = pygame.image.load("corps.png").convert_alpha()
-corps2 = pygame.transform.scale(corps2, (25,25))
-fenetre.blit(corps2, (5,60))
-Personnage4 = pygame.image.load("corps.png").convert_alpha()
-Personnage4 = pygame.transform.scale(Personnage4, (25,25))
-fenetre.blit(Personnage4, (5,85))
+fruit = pygame.image.load("fruit.png").convert_alpha()
+fruit = pygame.transform.scale(fruit, (35,35))
 
-fruit = pygame.image.load("square.jpg").convert()
-fenetre.blit(fruit, (250,250))
+#On charge les objets dans le jeu
+fenetre.blit(head, (0,0))
+fenetre.blit(corps1, (-5,5))
 
-# On récupère sa position
+#On récupère leur position
 position_1 = head.get_rect()
 position_fruit = fruit.get_rect()
 
 x[0] = position_1.x
 y[0] = position_1.y
-
-score = 0
-length = 3
+position_fruit.x = randint(1,300)
+position_fruit.y = randint(1,300)
 
 #Rafraichissement de l'écran
 pygame.display.flip()
@@ -157,10 +148,11 @@ while(continuer):
         if collision(position_fruit.x, position_fruit.y, x[i], y[i],35,25):
         	position_fruit.x = randint(1,670)
         	position_fruit.y = randint(1,670)
-        	length = length + 1
+        	length = length + 2
+        	step = step + 0.5
         	score = score + 1
-		
-    #Vérifie si la tête du serpent ne touche pas le corps
+
+    # Vérifie si la tête du serpent ne touche pas le corps
     for i in range(2,length):
             if collision(x[0], y[0], x[i], y[i],0,0) and move_init:
                 continuer = False
@@ -168,8 +160,4 @@ while(continuer):
     disp_score(score)
     pygame.display.flip()
 
-# On re-colle la fenêtre
-fenetre.blit(couverture, (0,0))
-
-pygame.display.flip()
 pygame.quit()
