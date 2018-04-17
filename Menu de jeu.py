a14315fc372c98446bd382a3b9d64640ee2e8b3e
tlemenestrel <34300678+tlemenestrel@users.noreply.github.com>
2018-04-17 14:56:50 +0100

    Update Menu de jeu.py

--- Menu de jeu.py
+++ Menu de jeu.py
@@ -2,9 +2,9 @@
 
 import pygame
 from pygame.locals import *
-from Projet import *
 
 pygame.init()
+
 
 #Ouverture de la fenêtre Pygame (carré : largeur = hauteur)
 fenetre = pygame.display.set_mode((300, 300))
@@ -12,31 +12,22 @@ pygame.display.set_caption("Accueil")
 
 
 #BOUCLE PRINCIPALE
-continuer = 1
-while continuer:	
-	#Chargement et affichage de l'écran d'accueil
-	accueil = pygame.image.load("accueil.png").convert()
-	fenetre.blit(accueil, (0,0))
+continuer_accueil = 1
+while continuer_accueil:
+    accueil = pygame.image.load("accueil.png").convert()
+    fenetre.blit(accueil, (0,0))
 
 	#Rafraichissement
-	pygame.display.flip()
-
-	#On remet ces variables à 1 à chaque tour de boucle
-	continuer_jeu = 1
-	continuer_accueil = 1
-
-	#BOUCLE D'ACCUEIL
-	while continuer_accueil:
-	
-	    for event in pygame.event.get():
-                    if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
-                            continuer_accueil = 0
-                            continuer_jeu = 0
-                            continuer = 0
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+            continuer_accueil = 0
 				
 				
-                    elif event.type == KEYDOWN:
-                            if event.key == K_F1:
-                                    continuer_accueil = 0
-                                    Projet = open("Projet.py","b")
-                                    Projet.read()
+        elif event.type == KEYDOWN:
+            if event.key == K_a:
+                from Projet import*
+                
+                
+pygame.quit()
+
