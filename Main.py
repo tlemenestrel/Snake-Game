e7e8210a0187ad104a985c5bba3963b752c44b86
colin-lag <34300599+colin-lag@users.noreply.github.com>
2018-04-25 16:17:44 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -61,7 +61,7 @@ fenetre_rect = fenetre.get_rect()
 #La fenêtre de jeu est nommée
 pygame.display.set_caption("Snake")
 
-#Chargement d'un fond blanc avec lequel la fenêtre est remplit
+#Chargement d'un fond blanc avec lequel la fenêtre est remplie
 couverture = pygame.Surface(fenetre.get_size())
 couverture = couverture.convert()
 couverture.fill((250, 250, 250))
